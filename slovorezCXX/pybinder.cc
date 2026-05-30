55783ce7919e206aa167472a2e057baaea2656da
pubari <pubaridev@gmail.com>
2026-05-30 20:33:27 +0300

    Implement tokenizer for compound tokens recognition
    Implement tokenizer with recognition for hyphened words/numbers.
    Move Python binder from tokenizer.cc to pybinder.cc.

---
+++ slovorezCXX/pybinder.cc
@@ -0,0 +1,340 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include "token.h"
+#include "tokenizer.h"
+
+namespace py = pybind11;
+using namespace py::literals;
+
+constexpr size_t DEFAULT_BATCH_SIZE = 65536;
+constexpr size_t DEFAULT_TOKEN_MIN_LEN = 0;
+constexpr size_t DEFAULT_TOKEN_MAX_LEN = 512;
+constexpr uint64_t NO_FILTER_MASK = 0xFFFFFFFFFFFFFFFF;
+
+class FromTextTokenizer {
+private:
+    TokenizerContext tctx;
+    char* raw_text = nullptr;
+    size_t text_len = 0;
+    size_t text_pos = 0;
+    char* batch_str_buf = nullptr;
+    TokenType* batch_types_buf = nullptr;
+    size_t batch_size = DEFAULT_BATCH_SIZE;
+    size_t token_min_len = DEFAULT_TOKEN_MIN_LEN;
+    size_t token_max_len = DEFAULT_TOKEN_MAX_LEN;
+    uint64_t filter_mask = NO_FILTER_MASK;
+
+public:
+    FromTextTokenizer(const char* str, size_t str_len) : text_len(str_len), text_pos(0)
+    {
+        this->raw_text = (char*)malloc(str_len);
+        this->batch_str_buf = (char*)malloc((512 * this->batch_size + this->batch_size) * sizeof(char));
+        this->batch_types_buf = (TokenType*)malloc(this->batch_size * sizeof(TokenType));
+        memcpy(this->raw_text, str, str_len);
+        slovorez_tokenizer_init(&this->tctx);
+    }
+
+    void set_batch_size(size_t batch_size)
+    {
+        this->batch_size = batch_size;
+        this->batch_str_buf = (char*)realloc(this->batch_str_buf, (512 * this->batch_size + this->batch_size) * sizeof(char));
+        this->batch_types_buf = (TokenType*)realloc(this->batch_types_buf, this->batch_size * sizeof(TokenType));
+    }
+
+    void set_filter(uint64_t filter_mask)
+    {
+        this->filter_mask = filter_mask;
+    }
+
+    void set_token_min_len(size_t token_min_len)
+    {
+        this->token_min_len = token_min_len;
+    }
+
+    void set_token_max_len(size_t token_max_len)
+    {
+        this->token_max_len = token_max_len;
+    }
+
+    py::dict get_batch()
+    {
+        size_t batch_str_size = 0;
+        size_t batch_token_idx = 0;
+        while (this->text_pos <= this->text_len && batch_token_idx <= this->batch_size)
+        {
+            if (slovorez_tokenizer_token_get(&this->tctx, (unsigned char)this->raw_text[this->text_pos++]))
+            {
+                const Token& token = this->tctx.rtoken;
+                const bool allowed_type = slovorez_token_filter_match(token.type, this->filter_mask);
+                const bool allowed_size = this->token_min_len <= token.size && token.size <= this->token_max_len;
+                if (allowed_type && allowed_size)
+                {
+                    for (int i = 0; i < token.size; ++i)
+                    {
+                        memcpy(this->batch_str_buf + batch_str_size, token.data[i].bytes, token.data[i].size);
+                        batch_str_size += token.data[i].size;
+                    }
+                    this->batch_str_buf[batch_str_size++] = '\0';
+                    this->batch_types_buf[batch_token_idx++] = token.type;
+                }
+            }
+        }
+        if (batch_token_idx == 0)
+        {
+            return py::dict();
+        }
+        py::dict outbuf;
+        outbuf["text"_s] = py::str(this->batch_str_buf, batch_str_size);
+        outbuf["types"_s] = py::array_t<uint64_t>(
+            { (size_t)batch_token_idx },
+            { sizeof(uint64_t) },
+            reinterpret_cast<uint64_t*>(this->batch_types_buf),
+            py::cast(this)
+        );
+        return outbuf;
+    }
+
+    ~FromTextTokenizer()
+    {
+        if (this->raw_text != nullptr)
+        {
+            free(this->raw_text);
+            this->raw_text = nullptr;
+        }
+        if (this->batch_str_buf != nullptr)
+        {
+            free(this->batch_str_buf);
+            this->batch_str_buf = nullptr;
+        }
+        if (this->batch_types_buf != nullptr)
+        {
+            free(this->batch_types_buf);
+            this->batch_types_buf = nullptr;
+        }
+    }
+};
+
+class FromFileTokenizer {
+private:
+    TokenizerContext tctx;
+    FILE* f = nullptr;
+    char* batch_str_buf = nullptr;
+    TokenType* batch_types_buf = nullptr;
+    size_t batch_size = DEFAULT_BATCH_SIZE;
+    size_t token_min_len = DEFAULT_TOKEN_MIN_LEN;
+    size_t token_max_len = DEFAULT_TOKEN_MAX_LEN;
+    uint64_t filter_mask = NO_FILTER_MASK;
+
+public:
+    FromFileTokenizer(const std::string& fpath)
+    {
+        this->f = fopen(fpath.c_str(), "r");
+        this->batch_str_buf = (char*)malloc((512 * this->batch_size + this->batch_size) * sizeof(char));
+        this->batch_types_buf = (TokenType*)malloc(this->batch_size * sizeof(TokenType));
+        slovorez_tokenizer_init(&this->tctx);
+    }
+
+    void set_batch_size(size_t batch_size)
+    {
+        this->batch_size = batch_size;
+        this->batch_str_buf = (char*)realloc(this->batch_str_buf, (512 * this->batch_size + this->batch_size) * sizeof(char));
+        this->batch_types_buf = (TokenType*)realloc(this->batch_types_buf, this->batch_size * sizeof(TokenType));
+    }
+
+    void set_filter(uint64_t filter_mask)
+    {
+        this->filter_mask = filter_mask;
+    }
+
+    void set_token_min_len(size_t token_min_len)
+    {
+        this->token_min_len = token_min_len;
+    }
+
+    void set_token_max_len(size_t token_max_len)
+    {
+        this->token_max_len = token_max_len;
+    }
+
+    bool is_fopen()
+    {
+        return this->f != nullptr;
+    }
+
+    py::dict get_batch()
+    {
+        if (this->f == nullptr)
+        {
+            return py::dict();
+        }
+        size_t batch_str_size = 0;
+        size_t batch_token_idx = 0;
+        int c;
+        while ((c = fgetc(this->f)) != EOF && batch_token_idx < this->batch_size)
+        {
+            if (slovorez_tokenizer_token_get(&this->tctx, (unsigned char)c))
+            {
+                const Token& token = this->tctx.rtoken;
+                const bool allowed_type = slovorez_token_filter_match(token.type, this->filter_mask);
+                const bool allowed_size = this->token_min_len <= token.size && token.size <= this->token_max_len;
+                if (allowed_type && allowed_size)
+                {
+                    for (int i = 0; i < token.size; ++i)
+                    {
+                        memcpy(this->batch_str_buf + batch_str_size, token.data[i].bytes, token.data[i].size);
+                        batch_str_size += token.data[i].size;
+                    }
+                    this->batch_str_buf[batch_str_size++] = '\0';
+                    this->batch_types_buf[batch_token_idx++] = token.type;
+                }
+            }
+        }
+
+        if (c == EOF && batch_token_idx < this->batch_size && slovorez_tokenizer_end(&this->tctx))
+        {
+            const Token& token = this->tctx.rtoken;
+            const bool allowed_type = slovorez_token_filter_match(token.type, this->filter_mask);
+            const bool allowed_size = this->token_min_len <= token.size && token.size <= this->token_max_len;
+            if (allowed_type && allowed_size)
+            {
+                for (int i = 0; i < token.size; ++i)
+                {
+                    memcpy(this->batch_str_buf + batch_str_size, token.data[i].bytes, token.data[i].size);
+                    batch_str_size += token.data[i].size;
+                }
+                this->batch_str_buf[batch_str_size++] = '\0';
+                this->batch_types_buf[batch_token_idx++] = token.type;
+            }
+        }
+
+        if (batch_token_idx == 0)
+        {
+            return py::dict();
+        }
+        py::dict outbuf;
+        outbuf["text"_s] = py::str(this->batch_str_buf, batch_str_size);
+        outbuf["types"_s] = py::array_t<uint64_t>(
+            { (size_t)batch_token_idx },
+            { sizeof(uint64_t) },
+            reinterpret_cast<uint64_t*>(this->batch_types_buf),
+            py::cast(this)
+        );
+        return outbuf;
+    }
+
+    ~FromFileTokenizer()
+    {
+        if (this->f != nullptr)
+        {
+            fclose(this->f);
+        }
+        if (this->batch_str_buf != nullptr)
+        {
+            free(this->batch_str_buf);
+            this->batch_str_buf = nullptr;
+        }
+        if (this->batch_types_buf != nullptr)
+        {
+            free(this->batch_types_buf);
+            this->batch_types_buf = nullptr;
+        }
+    }
+};
+
+typedef struct FromTextStream {
+    FromTextTokenizer &sentencer;
+    FromTextStream(FromTextTokenizer& s) : sentencer(s) {}
+} FromTextStream;
+
+typedef struct FromFileStream {
+    FromFileTokenizer &sentencer;
+    FromFileStream(FromFileTokenizer& s) : sentencer(s) {}
+} FromFileStream;
+
+PYBIND11_MODULE(slovorezCXX, m)
+{
+    py::enum_<TokenType>(m, "TokenType", py::arithmetic())
+        .value("NOTTKN", TokenType::NOTTKN)
+        .value("WRDSPC", TokenType::WRDSPC)
+        .value("NWLINE", TokenType::NWLINE)
+        .value("ENWORD", TokenType::ENWORD)
+        .value("NUMBER", TokenType::NUMBER)
+        .value("RUWORD", TokenType::RUWORD)
+        .value("PNCTTN", TokenType::PNCTTN)
+        .value("UNKNWN", TokenType::UNKNWN)
+        .export_values()
+        .def("__or__", [](TokenType a, TokenType b)
+            {
+                return static_cast<uint64_t>(a) | static_cast<uint64_t>(b);
+            }
+        )
+    ;
+
+    py::class_<FromTextStream>(m, "fts_stream")
+        .def("__iter__", [](FromTextStream &self) { return self; })
+        .def("__next__", [](FromTextStream &self)
+            {
+                py::dict batch = self.sentencer.get_batch();
+                if (batch.empty())
+                {
+                    throw py::stop_iteration();
+                }
+                return batch;
+            }
+        )
+    ;
+
+    py::class_<FromTextTokenizer>(m, "FTTokenizer")
+        .def(py::init([](const std::string& s)
+                {
+                    return new FromTextTokenizer(s.data(), s.size());
+                }
+            ),
+            py::arg("text")
+        )
+        .def("set_batch_size", &FromTextTokenizer::set_batch_size)
+        .def("set_filter", &FromTextTokenizer::set_filter)
+        .def("set_token_min_len", &FromTextTokenizer::set_token_min_len)
+        .def("set_token_max_len", &FromTextTokenizer::set_token_max_len)
+        .def("get_batch", &FromTextTokenizer::get_batch)
+        .def_property_readonly("stream", [](FromTextTokenizer& self)
+            {
+                return FromTextStream(self);
+            }
+        )
+    ;
+
+    py::class_<FromFileStream>(m, "ffs_stream")
+        .def("__iter__", [](FromFileStream &self) { return self; })
+        .def("__next__", [](FromFileStream &self)
+            {
+                py::dict batch = self.sentencer.get_batch();
+                if (batch.empty())
+                {
+                    throw py::stop_iteration();
+                }
+                return batch;
+            }
+        )
+    ;
+
+    py::class_<FromFileTokenizer>(m, "FFTokenizer")
+        .def(py::init<const std::string&>(), py::arg("fpath"))
+        .def("is_fopen", &FromFileTokenizer::is_fopen)
+        .def("set_batch_size", &FromFileTokenizer::set_batch_size)
+        .def("set_filter", &FromFileTokenizer::set_filter)
+        .def("set_token_min_len", &FromFileTokenizer::set_token_min_len)
+        .def("set_token_max_len", &FromFileTokenizer::set_token_max_len)
+        .def("get_batch", &FromFileTokenizer::get_batch)
+        .def_property_readonly("stream", [](FromFileTokenizer& self)
+            {
+                return FromFileStream(self);
+            }
+        )
+    ;
+}
